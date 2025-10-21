6ec787092808eb543fff374ac263bdc6dc08bb56
Denis T. <deadspell21391@gmail.com>
2025-10-21 22:46:09 +0200

    Form1.Designer.vb

---
+++ Smart Screen Bypass/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.AnotherSiteLinkCheckBox = New System.Windows.Forms.CheckBox()
+        Me.CheckBoxBatUsingHtml = New System.Windows.Forms.CheckBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.HtmlPayloadLink = New System.Windows.Forms.TextBox()
+        Me.numFilesCount = New System.Windows.Forms.NumericUpDown()
+        Me.TextBoxMshtaFormMessage = New System.Windows.Forms.TextBox()
+        Me.ChkMultipleFiles = New System.Windows.Forms.CheckBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.PageSiteTitleTextBox = New System.Windows.Forms.TextBox()
+        Me.FlatButton1 = New System.Windows.Forms.Button()
+        Me.AnotherSiteLinktextbox = New System.Windows.Forms.TextBox()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numFilesCount, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AnotherSiteLinkCheckBox
+        '
+        Me.AnotherSiteLinkCheckBox.AutoSize = True
+        Me.AnotherSiteLinkCheckBox.Location = New System.Drawing.Point(35, 88)
+        Me.AnotherSiteLinkCheckBox.Name = "AnotherSiteLinkCheckBox"
+        Me.AnotherSiteLinkCheckBox.Size = New System.Drawing.Size(150, 17)
+        Me.AnotherSiteLinkCheckBox.TabIndex = 0
+        Me.AnotherSiteLinkCheckBox.Text = "AnotherSiteLinkCheckBox"
+        Me.AnotherSiteLinkCheckBox.UseVisualStyleBackColor = True
+        '
+        'CheckBoxBatUsingHtml
+        '
+        Me.CheckBoxBatUsingHtml.AutoSize = True
+        Me.CheckBoxBatUsingHtml.Location = New System.Drawing.Point(35, 42)
+        Me.CheckBoxBatUsingHtml.Name = "CheckBoxBatUsingHtml"
+        Me.CheckBoxBatUsingHtml.Size = New System.Drawing.Size(139, 17)
+        Me.CheckBoxBatUsingHtml.TabIndex = 2
+        Me.CheckBoxBatUsingHtml.Text = "CheckBoxBatUsingHtml"
+        Me.CheckBoxBatUsingHtml.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(244, 74)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {9000, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(78, 20)
+        Me.NumericUpDown1.TabIndex = 3
+        '
+        'HtmlPayloadLink
+        '
+        Me.HtmlPayloadLink.Location = New System.Drawing.Point(35, 179)
+        Me.HtmlPayloadLink.Name = "HtmlPayloadLink"
+        Me.HtmlPayloadLink.Size = New System.Drawing.Size(151, 20)
+        Me.HtmlPayloadLink.TabIndex = 4
+        Me.HtmlPayloadLink.Text = "HtmlPayloadLink"
+        '
+        'numFilesCount
+        '
+        Me.numFilesCount.Location = New System.Drawing.Point(244, 111)
+        Me.numFilesCount.Maximum = New Decimal(New Integer() {9000, 0, 0, 0})
+        Me.numFilesCount.Name = "numFilesCount"
+        Me.numFilesCount.Size = New System.Drawing.Size(78, 20)
+        Me.numFilesCount.TabIndex = 5
+        '
+        'TextBoxMshtaFormMessage
+        '
+        Me.TextBoxMshtaFormMessage.Location = New System.Drawing.Point(35, 205)
+        Me.TextBoxMshtaFormMessage.Name = "TextBoxMshtaFormMessage"
+        Me.TextBoxMshtaFormMessage.Size = New System.Drawing.Size(151, 20)
+        Me.TextBoxMshtaFormMessage.TabIndex = 6
+        Me.TextBoxMshtaFormMessage.Text = "TextBoxMshtaFormMessage"
+        '
+        'ChkMultipleFiles
+        '
+        Me.ChkMultipleFiles.AutoSize = True
+        Me.ChkMultipleFiles.Location = New System.Drawing.Point(35, 112)
+        Me.ChkMultipleFiles.Name = "ChkMultipleFiles"
+        Me.ChkMultipleFiles.Size = New System.Drawing.Size(102, 17)
+        Me.ChkMultipleFiles.TabIndex = 7
+        Me.ChkMultipleFiles.Text = "ChkMultipleFiles"
+        Me.ChkMultipleFiles.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(58, 257)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(191, 23)
+        Me.ProgressBar1.TabIndex = 8
+        '
+        'PageSiteTitleTextBox
+        '
+        Me.PageSiteTitleTextBox.Location = New System.Drawing.Point(34, 231)
+        Me.PageSiteTitleTextBox.Name = "PageSiteTitleTextBox"
+        Me.PageSiteTitleTextBox.Size = New System.Drawing.Size(151, 20)
+        Me.PageSiteTitleTextBox.TabIndex = 9
+        Me.PageSiteTitleTextBox.Text = "PageSiteTitleTextBox"
+        '
+        'FlatButton1
+        '
+        Me.FlatButton1.Location = New System.Drawing.Point(284, 251)
+        Me.FlatButton1.Name = "FlatButton1"
+        Me.FlatButton1.Size = New System.Drawing.Size(106, 23)
+        Me.FlatButton1.TabIndex = 10
+        Me.FlatButton1.Text = "FlatButton1"
+        Me.FlatButton1.UseVisualStyleBackColor = True
+        '
+        'AnotherSiteLinktextbox
+        '
+        Me.AnotherSiteLinktextbox.Location = New System.Drawing.Point(34, 153)
+        Me.AnotherSiteLinktextbox.Name = "AnotherSiteLinktextbox"
+        Me.AnotherSiteLinktextbox.Size = New System.Drawing.Size(152, 20)
+        Me.AnotherSiteLinktextbox.TabIndex = 11
+        Me.AnotherSiteLinktextbox.Text = "AnotherSiteLinktextbox"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(402, 286)
+        Me.Controls.Add(Me.AnotherSiteLinktextbox)
+        Me.Controls.Add(Me.FlatButton1)
+        Me.Controls.Add(Me.PageSiteTitleTextBox)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.ChkMultipleFiles)
+        Me.Controls.Add(Me.TextBoxMshtaFormMessage)
+        Me.Controls.Add(Me.numFilesCount)
+        Me.Controls.Add(Me.HtmlPayloadLink)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.CheckBoxBatUsingHtml)
+        Me.Controls.Add(Me.AnotherSiteLinkCheckBox)
+        Me.Name = "Form1"
+        Me.Text = "Smart Screen Bypass By MONSTERMC"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numFilesCount, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents AnotherSiteLinkCheckBox As CheckBox
+    Friend WithEvents CheckBoxBatUsingHtml As CheckBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents HtmlPayloadLink As TextBox
+    Friend WithEvents numFilesCount As NumericUpDown
+    Friend WithEvents TextBoxMshtaFormMessage As TextBox
+    Friend WithEvents ChkMultipleFiles As CheckBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents PageSiteTitleTextBox As TextBox
+    Friend WithEvents FlatButton1 As Button
+    Friend WithEvents AnotherSiteLinktextbox As TextBox
+End Class
